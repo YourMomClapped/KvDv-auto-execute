d351cb40bf4ccccd3306bb68e3cca6eba08e5cfb
YourMomClapped <91243291+YourMomClapped@users.noreply.github.com>
2022-07-18 16:29:31 +0930

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 Wait(10)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YourMomClapped/Translator/main/Main"))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
 
 if game.PlaceId == 6461766546 then
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/ttd1108/Script/master/aherosdestiny2'), true))()
