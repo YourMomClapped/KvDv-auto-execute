1de22cd22a1740e3964c69dee148c8698fa7da36
YourMomClapped <91243291+YourMomClapped@users.noreply.github.com>
2022-07-22 10:36:54 +0930

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,6 @@
 Wait(10)
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Gi7331/scripts/main/Emote.lua", true))()
 
-if game.PlaceId == 6461766546 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/ttd1108/Script/master/aherosdestiny2'), true))()
-
-elseif game.PlaceId == 155615604 then
+if game.PlaceId == 155615604 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YourMomClapped/PrisonLifee/main/main.lua"))()
 end
