2e99c4c4a80c2a9385702890505543bccc95e054
YourMomClapped <91243291+YourMomClapped@users.noreply.github.com>
2022-07-22 10:50:59 +0930

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Gi7331/scripts/main/Emote.lua", true))()
 
 if game.PlaceId == 155615604 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YourMomClapped/PrisonLifee/main/main.lua"))()
